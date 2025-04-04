080eb7e66be875ed6cbfcbadb3aef5b84d2c1dec
chieund <chieund.fat@gmail.com>
2025-04-04 14:41:24 +0700

    main:

---
+++ day_2/morning/12_pattern_matching/irrefutable_patterns.rs
@@ -0,0 +1,14 @@
+fn takes_tuple(tuple: (char, i32, bool)) {
+    let a = tuple.0;
+    let b = tuple.1;
+    let c = tuple.2;
+
+    let (a, b, c) = tuple;
+    let (_, b, c) = tuple;
+
+    let (.., c) = tuple;
+}
+
+fn main() {
+    takes_tuple(('a', 777, true));
+}
